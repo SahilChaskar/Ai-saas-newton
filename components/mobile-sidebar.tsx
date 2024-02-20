94731040dc8ac893a7f2a8e5cefb50e1d396f730
SahilChaskar <1132210491@mitwpu.edu.in>
2024-02-20 12:05:33 +0530

    NEWTON_AI V1.3

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,28 +1,39 @@
-"use client"
-import { Button } from "./ui/button";
-import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-import Sidebar from "./sidebar"
-import { useEffect,useState } from "react";
+"use client";
 
-const MobileSidebar =() =>{
-    const [isMounted, setIsMounted] = useState(false);
-    useEffect(()=>{
-        setIsMounted(true);
-    },[]);
-    
-    if(!isMounted){return null;}
-    return( 
+import { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
+
+export const MobileSidebar = ({
+  apiLimitCount = 0,
+  isPro = false
+}: {
+  apiLimitCount: number;
+  isPro: boolean;
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
     <Sheet>
-        <SheetTrigger>
+      <SheetTrigger>
         <Button variant="ghost" size="icon" className="md:hidden">
-            <Menu/>
+          <Menu />
         </Button>
-        </SheetTrigger>
-        <SheetContent side="left" className="p-0">
-             <Sidebar apiLimitCount={0}/>
-        </SheetContent>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+      </SheetContent>
     </Sheet>
-    );
-}
-export default MobileSidebar
+  );
+};
